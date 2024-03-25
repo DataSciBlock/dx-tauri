f62960039fed5d24378e8f3178b718635345cde9
Bunyasit Fang <wachaa1319@gmail.com>
2024-03-25 18:08:28 +0700

    install polars and chrono

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,25 +1,48 @@
+use std::fmt::format;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use chrono::prelude::*;
+use polars::prelude::*;
 use tauri_plugin_dialog::{DialogExt, FileResponse};
 use tauri_plugin_fs::FsExt;
 
 // return a file_path `Option`, or `None` if the user closes the dialog
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from me!", name)
+    // format!("Hello, {}! You've been greeted from me!", name)
+    df!(
+        "integer" => &[1, 2, 3],
+        "date" => &[
+                NaiveDate::from_ymd_opt(2025, 1, 1).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+                NaiveDate::from_ymd_opt(2025, 1, 2).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+                NaiveDate::from_ymd_opt(2025, 1, 3).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+        ],
+        "float" => &[4.0, 5.0, 6.0],
+        "string" => &["a", "b", "c"],
+    )
+    .unwrap()
+    .to_string()
 }
 #[tauri::command]
 fn pick(app_handle: tauri::AppHandle) -> () {
     print!("pick() called from Rust, showing dialog to pick a file...");
 
-    let file_path = app_handle
+    let file = app_handle
         .dialog()
         .file()
+        .add_filter("csv files", &["csv"])
         .pick_file(|file_path: Option<FileResponse>| {
             match file_path {
                 Some(file_response) => {
                     println!("picked file: {:?}", file_response);
                     // Read the file
                     print!("{}", file_response.path.to_str().unwrap().to_string());
+
+                    let df = CsvReader::from_path(file_response.path.to_str().unwrap().to_string())
+                        .unwrap()
+                        .finish()
+                        .unwrap();
+                    println!("{}", df);
                 }
                 None => {
                     println!("no file picked");
