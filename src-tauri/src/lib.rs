057e9aec804f14bc53f214ce2a2ecb5714e21ce5
Bunyasit Fang <wachaa1319@gmail.com>
2024-03-25 10:33:52 +0700

    Add tauri-plugin-fs and tauri-plugin-dialog dependencies

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,49 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use tauri_plugin_dialog::{DialogExt, FileResponse};
+use tauri_plugin_fs::FsExt;
+
+// return a file_path `Option`, or `None` if the user closes the dialog
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Hello, {}! You've been greeted from me!", name)
+}
+#[tauri::command]
+fn pick(app_handle: tauri::AppHandle) -> () {
+    print!("pick() called from Rust, showing dialog to pick a file...");
+
+    let file_path = app_handle
+        .dialog()
+        .file()
+        .pick_file(|file_path: Option<FileResponse>| {
+            match file_path {
+                Some(file_response) => {
+                    println!("picked file: {:?}", file_response);
+                    // Read the file
+                    print!("{}", file_response.path.to_str().unwrap().to_string());
+                }
+                None => {
+                    println!("no file picked");
+                    "".to_string(); // Return an empty string if no file is picked
+                }
+            }
+        });
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .setup(|app| {
+            // allowed the given directory
+            let scope = app.fs_scope();
+            scope.allow_directory("/", false);
+            dbg!(scope.allowed());
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet, pick])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,35 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import { open } from "@tauri-apps/plugin-dialog";
+import { ask } from "@tauri-apps/plugin-dialog";
 
+// Open a dialog
+
+// Prints file path and name to the console
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
+  const [file, setFile] = useState(""); // [path, name, size, type, lastModified
   const [name, setName] = useState("");
 
   async function greet() {
+    // const answer = await ask("This action cannot be reverted. Are you sure?", {
+    //   title: "Tauri",
+    //   kind: "warning",
+    //   // type: "warning",
+    // });
+    // console.log(answer);
+
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
+  }
+
+  async function pick() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    // await invoke("pick");
+    // setGreetMsg(await invoke("greet", { name }));
+    // setFile(await invoke("pick"));
+    invoke("pick");
   }
 
   return (
@@ -17,23 +38,33 @@ function App() {
       <h1>Welcome to Tauri!</h1>
 
       <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
+        {/* <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
+        </a> */}
+        {/* <a href="https://tauri.app" target="_blank">
           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
+        </a> */}
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      <button
+        onClick={() => {
+          console.log("clicked");
+          pick();
+        }}
+      >
+        Pick a file
+      </button>
+
+      {/* <p>Click on the Tauri, Vite, and React logos to learn more.</p> */}
 
       <form
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
+          // pick();
           greet();
         }}
       >
